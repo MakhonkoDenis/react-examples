192ce5b81b43bb67e53493ea64c108ce3d8f16af
MakhonkoDenis <makhonkodenis@gmail.com>
2016-09-27 23:56:49 +0300

    UPD: home work 3

--- homework/homework-3/js/notes-app.js
+++ homework/homework-3/js/notes-app.js
@@ -4,36 +4,56 @@
 					id: 1,
 					name: 'Denis',
 					phoneNumber: '+38096751343',
-					image: 'https://avatars0.githubusercontent.com/u/4941839?v=3&s=466'
+					image: 'https://avatars0.githubusercontent.com/u/4941839?v=3&s=466',
+					description: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla '
 				},
 				{
 					id: 2,
 					name: 'Stepan',
 					phoneNumber: '+38096751343',
-					image: 'https://avatars2.githubusercontent.com/u/6247069?v=3&s=400'
+					image: 'https://avatars2.githubusercontent.com/u/6247069?v=3&s=400',
+					description: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla '
 				},
 				{
 					id: 3,
 					name: 'Andrey',
 					phoneNumber: '+38096751343',
-					image: 'https://avatars3.githubusercontent.com/u/4987981?v=3&s=400'
+					image: 'https://avatars3.githubusercontent.com/u/4987981?v=3&s=400',
+					description: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla '
 				},
 				{
 					id: 4,
 					name: 'Dimon',
 					phoneNumber: '+38096751343',
-					image: 'https://avatars2.githubusercontent.com/u/2461627?v=3&s=400'
+					image: 'https://avatars2.githubusercontent.com/u/2461627?v=3&s=400',
+					description: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla '
 				}
 			];
 
 	var Contact = React.createClass( {
+			getInitialState: function() {
+				return {
+					isOpen: false,
+				};
+			},
 			render: function() {
 				return (
-					<li className="contact">
+					<li className="contact" onClick={ this.onClickHandler }>
 						<img src={ this.props.url } alt={this.props.name} />
-						<span className="user-name">{ this.props.name }</span>
+						<h2 className="user-name">{ this.props.name }</h2>
+						<i className="icon fa fa-arrow-down" aria-hidden="true"></i>
+							{ this.state.isOpen ? <div>{ this.props.phone }</div> : '' }
+							{ this.state.isOpen ? <div className='description'>{ this.props.description }</div> : '' }
 					</li>
 				);
+			},
+			onClickHandler:function() {
+				var newState = ( this.state.isOpen ) ? false : true ;
+
+				console.log(newState);
+				this.setState({
+					isOpen: newState
+				})
 			}
 		} ),
 		ContactList = React.createClass( {
@@ -47,12 +67,12 @@
 					<ul className="contact-list">
 						{
 							this.props.contacts.map( function( element ){
-								console.log(element.id);
-								console.log(element.image);
 								return <Contact
 									key = { element.id }
 									name = { element.name }
 									url = { element.image }
+									phone = { element.phoneNumber }
+									description = { element.description }
 									/>;
 							} )
 						}
